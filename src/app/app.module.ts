c0c368b7b28ea9d62e204603c6ccb4582b9a1d1f
gurezo <akihiko.kigure@gmail.com>
2021-05-06 21:25:23 +0900

    package add.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { NgxsModule } from '@ngxs/store';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
